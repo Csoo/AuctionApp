162b5fb2d7446ac7f3efe2402cd3750fbfb2ac58
Petras <petras.botond@gmail.com>
2020-03-13 13:11:13 +0100

    add server init

---
+++ server/src/main.cpp
@@ -0,0 +1,8 @@
+//
+// Created by petras on 2020. 03. 13..
+//
+
+int main (int argc, char* argv[]) {
+
+    return 0;
+}
---
+++ server/src/server/server.cpp
@@ -0,0 +1,5 @@
+//
+// Created by petras on 2020. 03. 13..
+//
+
+#include "server.h"
