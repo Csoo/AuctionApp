08a75faa4457de37198731b0e75b9978262f4a86
Csongor <csocso96@gmail.com>
2020-05-19 20:45:08 +0200

    Quick fix

--- client/apirequest.h
+++ client/apirequest.h
@@ -30,7 +30,7 @@ public:
     Q_INVOKABLE bool registerRequest(const QString &name, const QString &pw,  const QString &email,  const QString &fullName,  const QString &address,  const QString &phone = "");
     Q_INVOKABLE int addAuctionRequest(int userId, const QString &title, const QString &descriptionText, const QString &color, int currentPrice, int minStep, int categoryId,  int conditionId, QStringList tags, QString endDate, const QList<QByteArray> &images);
     Q_INVOKABLE int bidRequest(int auctionId, int licitUserId, int currentPrice, int bid);
-    Q_INVOKABLE int rateUserRequest(int userIdFrom, int userIdTo, bool isPositive, const QString &message);
+    Q_INVOKABLE int rateUserRequest(int rateId, bool isPositive, const QString &message);
     Q_INVOKABLE bool allAuctionRequest();
     QVector<AuctionItem> searchRequest(const QString &searchText, int category_id, const QString &color, int condition_id, int minPrice, int maxPrice, const QStringList &tags);
     QJsonDocument ownProfileRequest(int id);
